9213b29c4bf5b3e902c12a6fbfd76a7c2d0e94e3
Marcelo <santosmarcelo1995@live.com>
2020-12-26 10:57:30 -0300

    Refat Avaliador class/add tests

--- tests/Service/AvaliadorTest.php
+++ tests/Service/AvaliadorTest.php
@@ -44,4 +44,38 @@ class AvaliadorTest extends TestCase
 
         self::assertEquals(2500, $maiorValor);
     }
+
+    public function testAvaliadorDeveEncontrarOMenorValorDeLancesEmOrdemCrescente()
+    {
+        $leilao = new Leilao('Fiat 147 0KM');
+
+        $maria = new Usuario('Maria');
+        $joao = new Usuario('Joao');
+
+        $leilao->recebeLance(new Lance($joao, 2000));
+        $leilao->recebeLance(new Lance($maria, 2500));
+
+        $leiloeiro = new Avaliador();
+        $leiloeiro->avalia($leilao);
+        $menorValor = $leiloeiro->getMenorValor();
+
+        self::assertEquals(2000, $menorValor);
+    }
+
+    public function testAvaliadorDeveEncontrarOMenorValorDeLancesEmOrdemDecrescente()
+    {
+        $leilao = new Leilao('Fiat 147 0KM');
+
+        $maria = new Usuario('Maria');
+        $joao = new Usuario('Joao');
+
+        $leilao->recebeLance(new Lance($maria, 2500));
+        $leilao->recebeLance(new Lance($joao, 2000));
+
+        $leiloeiro = new Avaliador();
+        $leiloeiro->avalia($leilao);
+        $menorValor = $leiloeiro->getMenorValor();
+
+        self::assertEquals(2000, $menorValor);
+    }
 }
