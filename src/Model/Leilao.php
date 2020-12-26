542020fad95caf65fcade8bf885e4a77413be47b
Marcelo <santosmarcelo1995@live.com>
2020-12-26 13:28:02 -0300

    Add limit verification

--- src/Model/Leilao.php
+++ src/Model/Leilao.php
@@ -20,6 +20,12 @@ class Leilao
         if (!empty($this->lances) && $this->ehDoUltimoUsuario($lance)) {
             return;
         }
+
+        $totalLancesUsuario = $this->getQtdLancesUsuario($lance);
+        if ($totalLancesUsuario >= 5) {
+            return;
+        }
+
         $this->lances[] = $lance;
     }
 
@@ -36,5 +42,23 @@ class Leilao
         $ultimoLance = $this->lances[count($this->lances) - 1];
 
         return $lance->getUsuario() == $ultimoLance->getUsuario();
+    }
+
+    private function getQtdLancesUsuario(Lance $lance) : int
+    {
+        $usuario = $lance->getUsuario();
+        $totalLancesUsuario = array_reduce(
+            $this->lances, 
+            function (int $totalAcumulado, Lance $lanceAtual) use ($usuario) {
+                if ($lanceAtual->getUsuario() == $usuario) {
+                    return $totalAcumulado + 1;
+                }
+
+                return $totalAcumulado;
+            },
+            0
+        );
+
+        return $totalLancesUsuario;
     }
 }
